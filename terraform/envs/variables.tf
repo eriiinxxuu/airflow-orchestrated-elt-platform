f38239b23ddeb386c742b990bd81cada3463bd7e
eriiinxxuu <erinxxuu@gmail.com>
2026-03-28 12:53:21 +1000

    add variables

--- terraform/envs/variables.tf
+++ terraform/envs/variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "Deployment environment (dev / staging / prod)"
   type        = string
-
+  default = "dev"
   validation {
     condition     = contains(["dev", "staging", "prod"], var.environment)
     error_message = "environment must be dev, staging, or prod."
